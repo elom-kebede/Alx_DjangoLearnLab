87e8a20729d2ee50b6f2b85b0ef65ac2512fdb6f
elom-kebede <kebedeelias30@gmail.com>
2025-02-15 00:52:53 -0800

    just try

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$=9v6by7t!a-g^d(b1cae*mx(k3@d6er21t)m)y@dffkox5xk('
+SECRET_KEY = 'django-insecure-e*o262chv+u^bewjmw+198q1+_%u3hgent(vhj=n&!hg8wka39'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
