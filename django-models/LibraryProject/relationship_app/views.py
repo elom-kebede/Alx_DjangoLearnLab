99f0b8854c464400ae07dfad62fa5fea647633ad
elom-kebede <kebedeelias30@gmail.com>
2025-02-22 06:41:28 -0800

    add 'from django.contrib.auth.forms import UserCreationForm' this module

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,7 @@ from django.views.generic.detail import DetailView
 from .models import Library
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
