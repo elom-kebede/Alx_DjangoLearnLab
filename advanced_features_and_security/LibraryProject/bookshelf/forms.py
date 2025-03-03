efa7dd586b59e2cf9aedc9b94b459e06e312da1f
elom-kebede <kebedeelias30@gmail.com>
2025-03-03 13:52:23 -0800

    add example form to the form.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -4,10 +4,15 @@ from .models import Book
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['title', 'author']  # Include the fields you want to display in the form
-    
+        fields = ['title', 'author', 'publication_year']
+
     def clean_title(self):
         title = self.cleaned_data.get('title')
         if '<script>' in title:  # Basic XSS prevention
             raise forms.ValidationError("Invalid input detected.")
         return title
+    
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
