715506d4b186f9b819ba2955ec376db50923f031
elom-kebede <kebedeelias30@gmail.com>
2025-03-20 23:48:44 -0800

    Adding Comment Functionality to Blog Posts

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,7 +6,8 @@ from .views import (
     PostDetailView,
     PostCreateView,
     PostUpdateView,
-    PostDeleteView
+    PostDeleteView,
+    add_comment, edit_comment, delete_comment
 )
 
 urlpatterns = [
@@ -20,4 +21,8 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
+    path("posts/<int:post_id>/comments/new/", add_comment, name="add-comment"),
+    path("comments/<int:comment_id>/edit/", edit_comment, name="edit-comment"),
+    path("comments/<int:comment_id>/delete/", delete_comment, name="delete-comment"),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,13 +1,11 @@
-from django.shortcuts import render
-
 # Create your views here.
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from .forms import RegisterForm,PostForm
+from .forms import RegisterForm,PostForm, CommentForm
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Post,Comment
 
 def register(request):
     if request.method == "POST":
@@ -37,6 +35,7 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
 
 # Create a new post
 class PostCreateView(LoginRequiredMixin, CreateView):
@@ -71,3 +70,38 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
+    
+
+@login_required
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect("post-detail", pk=post.id)
+    else:
+        form = CommentForm()
+    return render(request, "blog/add_comment.html", {"form": form})
+
+@login_required
+def edit_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id, author=request.user)
+    if request.method == "POST":
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect("post_detail", pk=comment.post.id)
+    else:
+        form = CommentForm(instance=comment)
+    return render(request, "blog/edit_comment.html", {"form": form})
+
+@login_required
+def delete_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id, author=request.user)
+    post_id = comment.post.id
+    comment.delete()
+    return redirect("post_detail", pk=post_id)
