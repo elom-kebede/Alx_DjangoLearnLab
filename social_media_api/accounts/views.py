d14c9ca802a0c37daba47c7e4669026137f6a484
elom-kebede <kebedeelias30@gmail.com>
2025-03-24 23:55:27 -0800

    Project Setup and User Authentication for a Social Media API

---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import update_last_login
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import generics, status
+from .models import CustomUser
+from .serializers import RegisterSerializer, UserSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        user = CustomUser.objects.get(username=request.data["username"])
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key, "user": UserSerializer(user).data}, status=status.HTTP_201_CREATED)
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+        
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+            update_last_login(None, user)
+            return Response({"token": token.key, "user": UserSerializer(user).data})
+        return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
+
+
